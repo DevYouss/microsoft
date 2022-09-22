6e659647fb97e0be15878aac54fdb2ca1c85b46c
Nino <ntorres-ulisses@turgot-paris.info>
2022-09-22 13:00:32 +0200

    creation backend

--- connexion.php
+++ connexion.php
@@ -1,3 +1,21 @@
 <?php
 
+$connexionPDO = new PDO(
+    'mysql:host=localhost;dbname=microsoft;charset=utf8',
+    'root',
+    'root'
+);
 
+if(isset($_POST["connexion"])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $creation = $connexionPDO->prepare("INSERT INTO utilisateur (email, password) VALUES (:email, :password)");
+
+    $creation->bindValue(":email", $email);
+    $creation->bindValue(":password", $password);
+
+    $creation->execute();
+    
+    header("Location: index.php");
+}
